95f536d6900363139c72efd869995a883dbe2da3
mmilly <michael.milbank92@gmail.com>
2016-09-11 21:17:37 -0400

    .

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -21,9 +21,9 @@ class RtmEventHandler(object):
             IMIDs.remove('USLACKBOT')
         #print IMIDs
         for chan in IMIDs:
-        #self.msg_writer.send_message(chan,"testing this")
-            payload={'token':self.token,'channel':chan,'text':"testtesttest"}
-            requests.get('https://slack.com/api/chat.postMessage',params=payload)   
+            self.msg_writer.send_message(chan,"testing this")
+            #payload={'token':self.token,'channel':chan,'text':"testtesttest"}
+            #requests.get('https://slack.com/api/chat.postMessage',params=payload)   
     def handle(self, event):
 
         if 'type' in event:
