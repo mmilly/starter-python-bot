667bb517d39270f7d053a38414143adb257ed5bb
mmilly <michael.milbank92@gmail.com>
2016-09-11 22:08:35 -0400

    .

--- bot/event_handler.py
+++ bot/event_handler.py
@@ -12,7 +12,7 @@ class RtmEventHandler(object):
     def __init__(self, slack_clients, msg_writer):
         self.clients = slack_clients
         self.msg_writer = msg_writer
-        
+        print "trollolol"
         os.system("pip install schedule")
         
         
